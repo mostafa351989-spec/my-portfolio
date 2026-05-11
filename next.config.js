f8af02a710c4f0507fa879f0d7ca4f481cb7126a
اسمك <@example.com>
2026-05-11 09:46:52 +0300

    fix: disable edge runtime for jsonwebtoken

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['jsonwebtoken'],
+  },
+}
+
 module.exports = nextConfig

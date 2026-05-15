f5cea40449c9bde76948feaa9c1a64601bca96cf
Mostafa <@gmail.com>
2026-05-15 20:22:20 +0300

    Force remove admin folder to fix build

--- app/api/logout/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-export async function POST() {
-  const res = NextResponse.redirect(new URL('/admin/login'));
-  res.cookies.delete('admin_token');
-  return res;
-}
--- app/api/projects/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-dynamic'
-export const runtime = 'nodejs'
-
-
-import { NextResponse } from 'next/server';
-import { dbConnect } from '@/lib/dbConnect';
-import Project from '@/lib/models/Project';
-export async function GET() { await dbConnect(); const projects = await Project.find(); return NextResponse.json(projects); }
-export async function POST(req: Request) { await dbConnect(); const data = await req.json(); const project = await Project.create(data); return NextResponse.json(project); }

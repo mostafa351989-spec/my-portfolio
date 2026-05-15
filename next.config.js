76c4b5e460181ea6d92a58fd4800dabe66433e84
Mostafa <@gmail.com>
2026-05-15 18:55:38 +0300

    Clean next.config.js - remove experimental flags

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['jsonwebtoken'],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
